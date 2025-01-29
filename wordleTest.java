87113174dc23dc43562954fe9ce4352d35f0af16
Abdulkarim <157996344+Abdulkarim-N@users.noreply.github.com>
2025-01-29 09:58:14 -0500

    Update wordleTest.java

--- wordleTest.java
+++ wordleTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.Test;
 
 public class test{ //assume user guesses string, if found calls method to check, if the check returns true, then test passes
-
+//this test checks to see if the target word is actually randomized and is one of the words in a list of valid words that is given.
     @Test
     public void RandomTargetWordTest(){ //check to see if random target word words
 
@@ -22,7 +22,7 @@ public class test{ //assume user guesses string, if found calls method to check,
 
         assertTrue(validWords.contains(randomword)); //checks if the random word is in the list so that its not un-random
     }
-
+//this test checks to see if the input the user puts in is valid or not, same with validInput2
     @Test
     public void validInput1(){ //tests if the user can input guesses
 
@@ -45,7 +45,7 @@ public class test{ //assume user guesses string, if found calls method to check,
 
         assertTrue(valid); //should return true since tests is a valid word for input
     }
-
+//this test checks to see the maximum attemps at the start of the game to make sure that it always starts at 6
     @Test // assuming the game words where you start initially with 6 attemps, then decrease every time
     public void maxAttempts(){ //test to check if the game starts off with 6 attempts
 
@@ -54,7 +54,7 @@ public class test{ //assume user guesses string, if found calls method to check,
         assertEquals(6, getAttempts()); //so when the game starts, the amount of attempts should be equal to 6
 
     }
-
+//this test is to make sure that if the user guesses wrong the attemps actually go down
     @Test // assuming the game words where you start initially with 6 attemps, then decrease every time
     public void maxAttempts(){ //test to check if the game stops after 6 attempts
 
@@ -71,7 +71,7 @@ public class test{ //assume user guesses string, if found calls method to check,
         assertEquals(0, getAttempts()); //0 guesses remaining after 6 attempts
 
     }
-
+//this test checks to see after the game has 0 attemps the game will be over
     @Test // assuming the game words where you start initially with 6 attemps, then decrease every time
     public void gameOver(){ //test to check if the game stops after 6 attempts
 
@@ -89,7 +89,7 @@ public class test{ //assume user guesses string, if found calls method to check,
         assertTrue(gameend); //the gameend should be true to signify that there are zero attempts left
 
     }
-
+//this test checks to see if and once the game is over it actually outputs the expected message showing its over
     @Test // assuming the game words where you start initially with 6 attemps, then decrease every time, once its at 0, use a method to check if game is over
     // test should display the game over message, it should follow the format and say the games word was berry (only word in the List)
     public void DisplaysAnswer(){ //test to check if the game stops after 6 attempts
